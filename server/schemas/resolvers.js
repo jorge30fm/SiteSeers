232aba5bc7fe71960a7b8fc318056368f45fe48c
Jorge Felix <jorge30fm@gmail.com>
2022-09-13 09:06:59 -0400

    fix resolvers issue

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,34 +1,32 @@
-import { User, Campsite, Reservation } from "../models/index.js";
+import { User, Campsite } from "../models/index.js";
 import { AuthenticationError } from "apollo-server-express";
 import { signToken } from "../utils/auth.js";
 
 const resolvers = {
-	Query: {
-		me: async (parent, args, context) => {
-			if (context.user) {
-				const userData = await User.findOne({ _id: context.user._id }).select(
-					"-__v -password"
-				);
-				return userData;
-			}
-			throw new AuthenticationError("Not logged in");
-		},
-		users: async () => {
-			return User.find().select("-__v -password");
-		},
-		user: async (parent, { username }) => {
-			return User.findOne({ username }).select("-__v -password");
-		},
-		campsites: async () => {
-			return Campsite.find()
-				.select("-_v")
-				.populate("reviews")
-		}
-	},
-	Mutation: {
-		addUser: async (parent, args) => {
-			const user = await User.create(args);
-			const token = signToken(user);
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        );
+        return userData;
+      }
+      throw new AuthenticationError("Not logged in");
+    },
+    users: async () => {
+      return User.find().select("-__v -password");
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username }).select("-__v -password");
+    },
+    campsites: async () => {
+      return Campsite.find();
+    },
+  },
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
 
       return { token, user };
     },
