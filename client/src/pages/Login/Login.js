c374eefa8872f24da4959fc2ea4191a01baaab85
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-09-14 16:40:03 -0400

    cleanup complete

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,23 +1,32 @@
 import React from "react";
+import "./Login.css";
 
 const Login = () => {
   return (
     <main>
-      <section className="mountain-bg">
+      <section className="mountain-bg login-section">
         <h2>Login</h2>
-        <form>
+        <form className="flex-column">
+          <div className="login-div">
             <label>Email:</label>
             <input name="email" type="email"></input>
+          </div>
+          <div className="login-div">
             <label>Password:</label>
             <input name="password" type="password"></input>
-          <div className="btn-container">
-            <button type="submit">Login</button>
+          </div>
+          <div className="login-div">
+            <div className="btn-container">
+              <button type="submit">Login</button>
+            </div>
           </div>
         </form>
       </section>
-      <section className="centered">
+      <section className="login-section flex-column align-center">
         <h3>Not a user yet? No worries!</h3>
-        <button>Sign Up</button>
+        <div className="btn-container">
+          <button>Sign Up</button>
+        </div>
       </section>
     </main>
   );
