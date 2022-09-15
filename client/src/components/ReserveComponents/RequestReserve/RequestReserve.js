e83ae582d54db5c9a9fe22fbd60d0d8fe4a78aa2
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-09-14 22:00:03 -0400

    calender and request reserve components render on reserve site page

--- client/src/components/ReserveComponents/RequestReserve/RequestReserve.js
+++ client/src/components/ReserveComponents/RequestReserve/RequestReserve.js
@@ -4,20 +4,32 @@ import Carousel from "../../Carousel/Carousel";
 
 const RequestReserve = () => {
   return (
-  <div>
-    <Carousel />
-    <p>date start to date end</p>
-    <p>number of campers</p>
-    <p>nightly rate x number of nights</p>
-    <p>date --- $$$</p>
-    <p>date --- $$$</p>
-    <p>date --- $$$</p>
-    <p>total</p>
-    <section>
-        <p>total: $$$</p>
-        <button>Reserve</button>
-    </section>
-  </div>
+    <div>
+      <Carousel />
+      <section className="reserve-section flex-column align-start">
+        <h3>date start to date end</h3>
+        <h3>number of campers</h3>
+        <div>
+          <p>Nightly Rate x numOfNights</p>
+          {/* for each for div below */}
+          <div className="flex-row justify-space-between">
+            <p>insert date here</p>
+            <p>$$$</p>
+          </div>
+          <div className="flex-row justify-space-between">
+            <p>Total:</p>
+            <p>$$$</p>
+          </div>
+          <div className="flex-row justify-space-between">
+            <div className="flex-column align-center">
+              <p>Total:</p>
+              <p>$$$</p>
+            </div>
+            <button className="btn">Reserve</button>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
--- client/src/pages/ReserveSite/ReserveSite.js
+++ client/src/pages/ReserveSite/ReserveSite.js
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ReserveSite.css";
 import Calendar from "../../components/ReserveComponents/Calendar/Calendar";
 import RequestReserve from "../../components/ReserveComponents/RequestReserve/RequestReserve";
 
 const ReserveSite = () => {
-    return (
-        <main>
-            <Calendar />
-            <RequestReserve />
-        </main>
-        );
-    };
-    
-    export default ReserveSite;
+  const [renderCalendar, setRenderCalendar] = useState(false);
+  const [renderRequest, setRenderRequest] = useState(false);
+  return (
+    <main>
+      <Calendar
+        renderCalendar={renderCalendar}
+        setRenderCalendar={setRenderCalendar}
+      />
+      <RequestReserve
+        renderRequest={renderRequest}
+        setRenderRequest={setRenderRequest}
+      />
+    </main>
+  );
+};
+
+export default ReserveSite;
