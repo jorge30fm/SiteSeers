42232811f6966b43eef59f40efe763164bab9128
Chip Hill <hillkarl03@gmail.com>
2022-09-15 01:21:11 -0400

    changed spelling of ammenities to amenities in Campsite.js, added new sites.js file that includes amenities array

--- server/seeders/seeds.js
+++ server/seeders/seeds.js
@@ -1,6 +1,6 @@
 // const faker = require('faker');
-import users from './users.js';
-import sites from './sites.js';
+import users from './users.js'
+import sites from './sites.js'
 import db from '../config/connection.js';
 import { User, Campsite } from '../models/index.js';
 
@@ -13,17 +13,6 @@ db.once('open', async () => {
     await User.create(users);
     await Campsite.create(sites);
 
-    // for (let i = 0; i < thoughtSeeds.length; i++) {
-    //   const { _id, thoughtAuthor } = await Thought.create(thoughtSeeds[i]);
-    //   const user = await User.findOneAndUpdate(
-    //     { username: thoughtAuthor },
-    //     {
-    //       $addToSet: {
-    //         thoughts: _id,
-    //       },
-    //     }
-    //   );
-    // }
   } catch (err) {
     console.error(err);
     process.exit(1);
