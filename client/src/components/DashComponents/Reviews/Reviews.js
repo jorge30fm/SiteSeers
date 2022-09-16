cab3dcdd1a1e2536ddcba6c5e0fdcde40d5afc72
roo116 <roo116@nc.rr.com>
2022-09-16 06:00:13 -0400

    added createdAt date and did some formatting

--- client/src/components/DashComponents/Reviews/Reviews.js
+++ client/src/components/DashComponents/Reviews/Reviews.js
@@ -1,19 +1,19 @@
 import React from "react";
-import "./Review.css";
+import "./Reviews.css";
 import reviewData from "./reviewdata.js";
 // import ListingCard from "../../ListingCard/ListingCard";
 
 const Reviews = () => {
   return (
     <div>
-      <h3 className="dash-reserve-title">Reviews</h3>
+      <h3>Reviews</h3>
       {/* <a className="wrapper-link" href="/reservation-details"></a> */}
       {reviewData &&
         reviewData.map((review) => (
           <div key={review._id}>
             <h5>{review.campsite}</h5>
             <p>Rating: {review.rating}</p>
-            <p><span>{review.reviewText}</span></p>
+            <p>{review.reviewText} <span className="date-style">{Date(review.createdAt)}</span></p>
             <br></br>
           </div>
         ))}
