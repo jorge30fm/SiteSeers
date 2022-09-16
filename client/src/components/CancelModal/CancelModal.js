bf321c49458094f85df0dfecb397d240709076b2
dolcebasstrombone <dolcebasstrombone@gmail.com>
2022-09-15 20:59:15 -0400

    reserve info and cancel modal

--- client/src/components/CancelModal/CancelModal.js
+++ client/src/components/CancelModal/CancelModal.js
@@ -1,11 +1,25 @@
 import React from "react";
 import "./CancelModal.css";
 
-const CancelModal = () => {
-    return (
-        <div>
-            <div>Cancel</div>
-        </div>
-        );
-    };
-    export default CancelModal;
+const CancelModal = (props) => {
+  const { setModalOpen } = props;
+  return (
+    <div className="flex-column justify-content-center">
+      <h2>Are you sure you want to cancel your reservation?</h2>
+      <div className="btn-container">
+        <button className="btn">Cancel</button>
+      </div>
+      <div className="btn-container">
+        <button
+          className="btn btn-faded"
+          onClick={() => {
+            setModalOpen(false);
+          }}
+        >
+          Go back
+        </button>
+      </div>
+    </div>
+  );
+};
+export default CancelModal;
