ba8eb7610e812a40057a7ca2a279d290dbb35300
glo6al <towers.a@gmail.com>
2022-09-17 13:59:45 -0400

    initial privacy policy

---
+++ client/src/pages/Policies/PrivacyPolicy.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Policies.css";
+
+const TermsService = () => {
+  return (
+    <main>
+      <section className="justify-content-center">
+        <h2>Privacy Policy</h2>
+      </section>
+    </main>
+  );
+};
+
+export default TermsService;
