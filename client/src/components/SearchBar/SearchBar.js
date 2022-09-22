c6d684079a8d31cafb56c8385155b2cfbb096f19
Jorge Felix <jorge30fm@gmail.com>
2022-09-22 16:29:10 -0400

    style search page

--- client/src/components/SearchBar/SearchBar.js
+++ client/src/components/SearchBar/SearchBar.js
@@ -1,34 +1,42 @@
 import React from "react";
-
+import "./SearchBar.css";
 function SearchBar(props) {
-  const { setSearchState } = props;
+	const { setSearchState } = props;
 
-  // submit form
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setSearchState(document.querySelector("#search-bar").value);
-  };
+	// submit form
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		setSearchState(document.querySelector("#search-bar").value);
+	};
 
-  return (
-    <section className="mountain-bg">
-      <form>
-        <input
-          type="text"
-          placeholder="Find a New Adventure"
-          name="search"
-          id="search-bar"
-        />
-        <button
-          type="submit"
-          id="search-btn"
-          className="btn"
-          onClick={handleSubmit}
-        >
-          Search
-        </button>
-      </form>
-    </section>
-  );
+	return (
+		<section className="heroBg">
+			<div display="flex-row justify-center">
+				<h1 className="text-center mainTitle"><strong>SITE</strong><span>SEER</span></h1>
+				<h2 className="text-center margin-top"> A simpler way </h2>
+				<h2 className="text-center">to camp</h2>
+			</div>
+			<form className="top-margin flex-row formPadding">
+				<input
+					type="text"
+					placeholder="Find a New Adventure"
+					name="search"
+					id="search-bar"
+				/>
+				<div>
+					<button
+						type="submit"
+						id="search-btn"
+						className="btn btn-long"
+						onClick={handleSubmit}
+					>
+						Search
+					</button>
+				</div>
+				<h3 className="text-center margin-top col-12 font-larger">find campsites near you</h3>
+			</form>
+		</section>
+	);
 }
 
 export default SearchBar;
