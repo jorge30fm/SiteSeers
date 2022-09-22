59753326c2a76bc3f61540e9e0b5fd08b1beb11f
cheddar <nowaczewskijack@gmail.com>
2022-09-21 21:15:11 -0400

    remove star and add hardcoded campsite data

--- client/src/components/ListingCard/ListingCard.js
+++ client/src/components/ListingCard/ListingCard.js
@@ -2,21 +2,15 @@ import React from "react";
 import Carousel from "../Carousel/Carousel";
 import "./ListingCard.css";
 import StarIcon from "@mui/icons-material/Star";
-const ListingCard = () => {
+const ListingCard = (props) => {
+  // const { campsite } = props;
   return (
     <div className="listingCard">
       {/* TODO: On click, redirect to single campsite page */}
-      <Carousel />
+      <Carousel></Carousel>
       <div className="basicInfo">
-        <div>
-          <StarIcon />
-          <StarIcon />
-          <StarIcon />
-          <StarIcon />
-          <StarIcon />
-        </div>
         <h2>Name</h2>
-        <h3 className="location">Location</h3>
+        <h3 className="location">City, State</h3>
       </div>
     </div>
   );
