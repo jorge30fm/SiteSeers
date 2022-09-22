4436882217d6106bd4b4d858b1ec65a70d5c507b
cheddar <nowaczewskijack@gmail.com>
2022-09-21 22:34:58 -0400

    add functionality to carousel

--- client/src/components/Carousel/Carousel.js
+++ client/src/components/Carousel/Carousel.js
@@ -6,13 +6,42 @@ import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
 
 function Carousel(props) {
   const [currImg, setCurrImg] = useState(0);
-  const [hasImages, setHasImages] = useState(true);
+  // const [hasImages, setHasImages] = useState(true);
   const { campsite } = props;
   const imagesArray = [...campsite.images];
 
   if (!imagesArray.length) {
-    return <div className="carousel"></div>
-    // setHasImages(false)
+    return (
+      <div className="carousel">
+        <div
+          className="carouselInner"
+          style={{
+            backgroundColor: "var(--light-green)",
+          }}
+        >
+          <div
+            className="left carouselArrow"
+            onClick={() => {
+              currImg > 0 && setCurrImg(currImg - 1);
+            }}
+          >
+            <ArrowBackIosIcon />
+          </div>
+          <div className="center">
+            <BookmarkBorderIcon className="save" style={{ fontSize: 30 }} />
+            <h1 className="rate">${campsite.price}</h1>
+          </div>
+          <div
+            className="right carouselArrow"
+            onClick={() => {
+              currImg < imagesArray.length - 1 && setCurrImg(currImg + 1);
+            }}
+          >
+            <ArrowForwardIosIcon />
+          </div>
+        </div>
+      </div>
+    );
   }
 
   return (
