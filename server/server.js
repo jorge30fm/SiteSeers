0b3140df542c9de2e0fb845aa76c7ee0d8bda1ae
Chip Hill <hillkarl03@gmail.com>
2022-09-22 13:22:06 -0400

    removed appuse statements for dirname..   here goes nothing.

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import express from "express";
 import { ApolloServer } from "apollo-server-express";
 import { authMiddleware } from "./utils/auth.js";
 import path from "path";
+import {fileURLToPath} from 'url';
 import { typeDefs, resolvers } from "./schemas/index.js";
 import db from "./config/connection.js";
 import * as dotenv from "dotenv";
@@ -16,18 +17,22 @@ const server = new ApolloServer({
 });
 
 const app = express();
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = path.dirname(__filename);
+// console.log('directory-name 👉️', __dirname);
+// console.log(path.join(__dirname, '/dist', 'index.html'));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// });
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async (typeDefs, resolvers) => {
